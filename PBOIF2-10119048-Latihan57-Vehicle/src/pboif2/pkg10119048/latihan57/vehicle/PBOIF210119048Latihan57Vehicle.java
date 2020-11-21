738ec14ec829cf41ba9b3e36ffef1aaecdf941ec
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-11-21 20:25:56 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan57-Vehicle/src/pboif2/pkg10119048/latihan57/vehicle/PBOIF210119048Latihan57Vehicle.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan57.vehicle;
+
+/**
+ *
+ * @author SystemOs
+ */
+public class PBOIF210119048Latihan57Vehicle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bicycle bicycle = new Bicycle();
+        bicycle.setMyBrand("Trek Bike");
+        bicycle.setMyModel("7.4FX");
+        bicycle.setMyGearCount(23);
+        System.out.println("Brand : " + bicycle.getMyBrand());
+        System.out.println("Model : " + bicycle.getMyModel());
+        System.out.println("Jumlah Gear : " + bicycle.getMyGearCount());
+        System.out.println();
+        SkateBoard skate = new SkateBoard();
+        skate.setMyBrand("Ally Skate");
+        skate.setMyModel("Rocket");
+        skate.setMyBoardLength(54.5);
+        System.out.println("Brand : " + skate.getMyBrand());
+        System.out.println("Model : " + skate.getMyModel());
+        System.out.println("Panjang Board : " + skate.getMyBoardLength());
+    }
+}
---
+++ PBOIF2-10119048-Latihan57-Vehicle/src/pboif2/pkg10119048/latihan57/vehicle/Vehicle.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan57.vehicle;
+
+/**
+ *
+ * @author SystemOs
+ */
+public class Vehicle {
+    private String myBrand,myModel;
+
+    public Vehicle() {
+    }
+
+    public String getMyBrand() {
+        return myBrand;
+    }
+
+    public void setMyBrand(String myBrand) {
+        this.myBrand = myBrand;
+    }
+
+    public String getMyModel() {
+        return myModel;
+    }
+
+    public void setMyModel(String myModel) {
+        this.myModel = myModel;
+    } 
+}
